8213ff95f32b87f24405c45d73bace1bdcc2b17c
gineff <canone@inbox.ru>
2023-09-20 15:59:54 +0300

    chore: add initial settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:css-modules/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'css-modules'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-global-assign': 0,
+  },
+}
