2f7402f6f0bd5e7bf6a2f828d4305d169a7aff41
gineff <canone@inbox.ru>
2023-09-21 13:08:36 +0300

    ft: modify component

--- src/lib/component.js
+++ src/lib/component.js
@@ -1,19 +1,14 @@
 /* eslint-disable no-unused-vars */
-import { getContext, setContext, nextId } from '../utils/index.js'
+import { getContext, setContext, uid } from '../utils/index.js'
 import { useDom } from '../hooks/useDom.js'
 
-const re =
-  /<([A-Z][A-Za-z0-9._]+)\s*([^>]*)\/>|<(?<tag>[A-Z][A-Za-z0-9._]+)\s*([^>]*)>(.*?)<\/\k<tag>\s?>|context:(\d+)/
-const ternaryOperatorRe = /\{\{\s*([^}]*)\?(?!\.)\s*(.*?)\s*:\s*(.*?)\s*\}\}/g
+export const reComponent =
+  /<([A-Z][A-Za-z0-9._]+)\s*([^>]*)\s*\/>|<(?<tag>[A-Z][A-Za-z0-9._]+)\s*([^>]*)\s*>(.*?)<\/\k<tag>\s?>|context:(\d+)/
+const ternaryOperatorRe =
+  /\{\{\s*([^}]*)\s*\?\s+([\s\S]*?)\s*:\s+([\s\S]*?)\s*\}\}/gm
 const propsRegexp =
-  /(\w+)\s*=\s*((?<quote>["'`])(.*?)\k<quote>|context:(\d+))|(\w+)/g
+  /([\w\d-]+)\s*=\s*((?<quote>["'`])([\s\S]*?)\k<quote>|context:(\d+))|(\w+)/g
 const components = new Map()
-
-const eventMap = {
-  onClick: 'click',
-  onBlur: 'blur',
-  onChange: 'change',
-}
 
 function getValue(path, obj) {
   const keys = path.trim().split('.')
@@ -35,16 +30,23 @@ function getValue(path, obj) {
 }
 
 function parsePropsFromString(str) {
-  if (str === undefined) return undefined
-  if (str === '') return ''
-
-  const props = {}
+  let props = {}
   const matches = str.matchAll(propsRegexp)
 
   for (const match of matches) {
     const [, key, , , value, contextId, attribute] = match
-    props[attribute || key] =
-      attribute || (contextId ? getContext(contextId) : value)
+
+    if (attribute) {
+      props = { ...props, attribute }
+    } else if (contextId) {
+      const context = getContext(Number(contextId))
+      if (context.isComponent) {
+        continue
+      }
+      props = { ...props, [key]: context }
+    } else {
+      props = { ...props, [key]: value }
+    }
   }
   return props
 }
@@ -61,75 +63,57 @@ function isPrimitive(element) {
   return Object(element) !== element
 }
 
-function parseNestedComponents(block) {
-  let match
-  const collection = {}
-
-  while ((match = block.match(re))) {
-    const [
-      found,
-      singleTag,
-      singleTagProps,
-      pairedTag,
-      pairedTagProps,
-      children,
-      context,
-    ] = match
-    const id = nextId()
-    block = block.replace(
-      found,
-      `<div component-id="${id}">${children || ''}</div>`
-    )
-
-    if (context) {
-      collection[id] = getContext(context)
-    } else {
-      const props = parseProps(singleTagProps || pairedTagProps)
-      try {
-        collection[id] = new (components.get(singleTag || pairedTag))({
-          ...props,
-        })
-      } catch (e) {
-        console.log(e)
-        console.error(
-          singleTag || pairedTag,
-          components,
-          '-------',
-          components.get(singleTag || pairedTag)
-        )
-      }
-    }
-  }
-
-  return [block, collection]
+function registerComponent(key, value) {
+  components.set(key, value)
 }
 
 export default class Component {
-  constructor(props) {
-    this.element = document.createElement('div')
+  id = uid()
+  template = '<div>{{children}}</div>'
+  element = document.createElement('div')
+  state = {}
+  props = {}
+  static isComponent = true
+  refs = {}
 
-    Object.entries(props).forEach(([key, value]) => {
-      if (value && isComponent(value)) {
-        components.set(key, value)
+  constructor(props) {
+    for (const key in props) {
+      const value = props[key]
+
+      if (typeof value === 'function' && 'isComponent' in value) {
+        registerComponent(key, value)
       } else {
-        this.props[key] = typeof value === 'function' ? value.bind(this) : value
+        this.props[key] = value
       }
-    })
+    }
 
     this.getStateFromProps()
+    this._render()
   }
 
-  isComponent = true
-  props = {}
-  state = {}
-
-  setState(key, value) {
-    this.state[key] = value
+  setState = nextState => {
+    if (!nextState) {
+      return
+    }
+    Object.assign(this.state || ((this.state = {}), this.state), nextState)
   }
 
   _compile(template) {
-    if (!template) console.error(this.constructor.name, 'отсутствует шаблон')
+    if (!template) console.error(this.constructor.name, ' отсутствует шаблон')
 
+    template = template.replace(
+      /\{\{\s*([A-Za-z0-9._-]+)\s*\}\}/g,
+      (_match, key) => {
+        const value = getValue(key, this.state)
+        if (!value == undefined || value == null) {
+          return ' '
+        }
+        if (isPrimitive(value)) {
+          return String(value)
+        }
+        return `context:${setContext(value)}`
+      }
+    )
     template = template.replace(
       ternaryOperatorRe,
       (_match, condition, value1, value2) => {
@@ -139,18 +123,50 @@ export default class Component {
         return result.replace(/null|undefined/g, '')
       }
     )
+    return template
+  }
 
-    return template.replace(/\{\{([A-Za-z0-9._-]+)\}\}/g, (_match, key) => {
-      const value = getValue(key, this.state)
+  defineElement(newElement) {
+    this.element.replaceWith(newElement)
+    this.element = newElement
+  }
 
-      if (value == undefined || value == null) {
-        return ' '
+  decomposeBlock(block) {
+    let match
+    const collection = []
+
+    while ((match = block.match(reComponent))) {
+      const [
+        found,
+        singleTag,
+        singleTagProps,
+        pairedTag,
+        pairedTagProps,
+        children,
+        context,
+      ] = match
+
+      let component
+
+      if (context) {
+        component = getContext(Number(context))
+      } else {
+        const props = parseProps(singleTagProps || pairedTagProps)
+        component = new (components.get(singleTag || pairedTag))({
+          ...props,
+          children,
+        })
       }
-      if (isPrimitive(value)) {
-        return value
-      }
-      return `context:${setContext(value)}`
-    })
+
+      if (!component) continue
+
+      const id =
+        collection.push(Array.isArray(component) ? component : [component]) - 1
+      block = block.replace(found, `<div component-id="${id}" ></div>`)
+    }
+
+    const response = [block, collection]
+    return response
   }
 
   getStateFromProps() {
@@ -163,54 +179,55 @@ export default class Component {
   }
 
   render() {
-    const newElement = this._render()
-    this.element.replaceWith(newElement)
-    this.element = newElement
-    this.addEventHandler(this.element, this.state)
-    return this.element
+    return this.template
   }
 
   _render() {
-    const block = this._compile(this.template).replace(/\n|\s{2}/g, '')
+    const block = this._compile(this.render()).replace(/\n|\s{2}/g, '')
     this.block = block
-
-    const [htmlTree, nestedComponents] = parseNestedComponents(block)
-
+    const [htmlTree, nestedComponents] = this.decomposeBlock(block)
+    //console.log(htmlTree, nestedComponents)
     const dom = useDom(htmlTree)
-
-    Object.entries(nestedComponents).forEach(([id, componentOrChildNode]) => {
-      const domElement = dom.querySelector(`[component-id="${id}"]`)
-
-      if (domElement.childNodes.length > 0) {
-        componentOrChildNode.state = {
-          ...componentOrChildNode.state,
-          children: [...domElement.childNodes],
-        }
-      }
-
-      if (componentOrChildNode.isComponent) {
-        domElement.replaceWith(componentOrChildNode.render())
-      } else {
-        if (componentOrChildNode[0] && componentOrChildNode[0].isComponent) {
-          domElement.replaceWith(
-            ...componentOrChildNode.map(comp => comp.render())
-          )
-        } else {
-          domElement.replaceWith(...componentOrChildNode)
-        }
-      }
+    nestedComponents.forEach((nested, id) => {
+      const stub = dom.querySelector(`[component-id="${id}"]`)
+      if (!stub) return
+      stub.replaceWith(
+        ...nested.map(comp => {
+          return comp.getContent()
+        })
+      )
     })
-
-    return dom.getElement()
+    this.defineElement(dom.getElement())
+    this.addEventHandler(this.element, this.state)
   }
 
   addEventHandler(element, props) {
-    Object.entries(props).forEach(([key, handler]) => {
-      if (typeof handler !== 'function') return
-      const eventName = eventMap[key]
-      if (eventName) {
-        element.addEventListener(eventName, handler, { capture: true })
+    for (const key in props) {
+      const handler = props[key]
+      if (typeof handler !== 'function') continue
+      let match
+      if ((match = key.match(/^on(\w+)/))) {
+        const eventKey = match[1].toLowerCase()
+        element.addEventListener(eventKey, handler, { capture: true })
       }
-    })
+    }
+  }
+
+  getContent() {
+    return this.element
+  }
+
+  show() {
+    const content = this.getContent()
+    if (content instanceof HTMLElement) {
+      content.style.display = 'block'
+    }
+  }
+
+  hide() {
+    const content = this.getContent()
+    if (content instanceof HTMLElement) {
+      content.style.display = 'none'
+    }
   }
 }
